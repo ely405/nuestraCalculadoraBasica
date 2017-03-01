04f64dbd68b2fcaed59701ad5e388bdbab093c26
Neiza Nunez <neizapf@gmail.com>
2017-03-01 13:53:49 -0500

    primer commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
 
+
+
+function operacion (){
+  var suma       = document.getElementById('num1').value + document.getElementById('num2').value;
+  var resta      = document.getElementById('num1').value - document.getElementById('num2').value;
+  var multiplica = document.getElementById('num1').value * document.getElementById('num2').value;
+  var divide     = document.getElementById('num1').value / document.getElementById('num2').value;
+
+}
+function print (){
+  var record = document.getElementById('id');
+  algo == "suma"          ? r=suma:
+  algo == "resta"         ? r= resta:
+  algo =="multiplicacion" ? r=multiplica:
+  algo == "division"      ? r= divide: alert('mal');
+  record.innerHTMML= r;
+}
