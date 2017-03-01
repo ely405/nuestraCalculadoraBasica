6f621caf6be0c7fc9d1a85a6059b97b10e7b115a
Mishmore <michellemore28@gmail.com>
2017-03-01 14:18:29 -0500

    probando funciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,21 @@
-function suma(){
-  var sum = parseInt(document.getElementById('firstNumber').value) + parseInt(document.getElementById('secondNumber').value);
-  return document.getElementById('result').innerHTML = sum;
-}
-function resta(){
-  var res = parseInt(document.getElementById('firstNumber').value) - parseInt(document.getElementById('secondNumber').value);
-  return document.getElementById('result').innerHTML = res;
-}
 
-function imprimeResult() {
-  suma();
-}
+function imprimeResult(){
+  var nombre = document.getElementById("name").value;
+  var num1 = document.getElementById("firstNumber").value;
+  var num2 = document.getElementById("secondNumber").value;
+  var operacion = document.getElementById("operador").value;
+  var sum = parseInt(num1) + parseInt(num2);
+  var res = parseInt(num1) - parseInt(num2);
+  var div = parseInt(num1) / parseInt(num2);
+  var mult = parseInt(num1) * parseInt(num2);
 
-function continuaOperando() {
-  return suma();
+  var resultado = if (operacion == "suma"){
+                      return sum;
+                    } if (operacion == "resta"){
+                      res;
+                    } if (operacion == "division"){
+                     div;
+                   }  if (operacion == "multiplicacion"){
+                     mult;
+                   };
 }
-
-/*
-var result;
-function sum() {
-  return result;
-}
-
-switch (sum()) {
-    case "suma":
-        result = 4;
-        break;
-    case "resta":
-        result=6;
-        break;
-}
-document.getElementById("hola").innerHTML = "Today is " + day;
-*/
