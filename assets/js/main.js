705302f0aa0fbfc42678dcb80170aac077c94579
Leidy Rosmery <rosm3rita@hotmail.com>
2017-03-01 14:22:15 -0500

    mi segundo commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
 
+function continuaOperando(){
+  var nombre = document.getElementById("name").value;
+  var num1 = document.getElementById("firstNumber").value;
+  var num2 = document.getElementById("secondNumber").value;
+  var op = document.getElementById("operador").value;
+
+  var resultado = (op == "suma")? parseInt(num1)+parseInt(num2) :
+                  (op == "resta")? parseInt(num1)-parseInt(num2):
+                  (op == "division")? parseInt(num1)/parseInt(num2):
+                  (op == "multiplicacion")? parseInt(num1)*parseInt(num2): "";
+
+document.getElementById('firstNumber').value= resultado;
+document.getElementById('secondNumber').value="";
+document.getElementById('operador').value="";
+
+}
