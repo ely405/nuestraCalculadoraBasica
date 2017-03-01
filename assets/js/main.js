357785321c7cfb1ba37cd6862e6a058b4139a0d7
Fiorella Cisneros <fiorella.0624@gmail.com>
2017-03-01 14:19:47 -0500

    imprimeResult refactorizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
+function imprimeResult(){
+  var nombre = document.getElementById("name").value;
+  var num1 = document.getElementById("firstNumber").value;
+  var num2 = document.getElementById("secondNumber").value;
+  var op = document.getElementById("operador").value;
+  var all = document.getElementsByClassName('clase');
 
+  if (all[0].value.length == "" || all[1].value.length == 0 || all[2].value.length == 0 || all[3].value.length == 0  ){
+
+  }else{
+  var resultado = (op == "suma")? parseInt(num1)+parseInt(num2) :
+                  (op == "resta")? parseInt(num1)-parseInt(num2):
+                  (op == "division")? parseInt(num1)/parseInt(num2):
+                  (op == "multiplicacion")? parseInt(num1)*parseInt(num2): "";
+}
+  document.getElementById("result").innerHTML = "hola " + nombre + 
+  												" el resultado de tu operación es " + resultado;
+
+}
